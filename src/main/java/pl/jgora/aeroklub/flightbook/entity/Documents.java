01b934c43a3090592222bffc9699f878b030e36b
Marcin <awings@o2.pl>
2023-09-25 12:55:56 +0200

    Documents Entity

--- src/main/java/pl/jgora/aeroklub/flightbook/entity/Documents.java
+++ src/main/java/pl/jgora/aeroklub/flightbook/entity/Documents.java
@@ -18,7 +18,7 @@ public class Documents {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
-    private String descroption;
+    private String description;
     private LocalDate dateOfExpiry;
 
 }
