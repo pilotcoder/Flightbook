de581f760ddda024c594c780f260ee4953e71e75
pilotcoder <awings@o2.pl>
2023-09-28 08:03:35 +0200

    Documents repository find methods added

--- src/main/java/pl/jgora/aeroklub/flightbook/repository/DocumentsRepository.java
+++ src/main/java/pl/jgora/aeroklub/flightbook/repository/DocumentsRepository.java
@@ -2,7 +2,16 @@ package pl.jgora.aeroklub.flightbook.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import pl.jgora.aeroklub.flightbook.entity.Documents;
+import pl.jgora.aeroklub.flightbook.entity.LLP;
+
+import java.util.List;
 
 public interface DocumentsRepository extends JpaRepository<Documents, Long> {
+
+    public Documents findByName(String name);
+    public List<Documents> findDocumentsByGliderId (Long gliderId);
+
+
+
 
 }
