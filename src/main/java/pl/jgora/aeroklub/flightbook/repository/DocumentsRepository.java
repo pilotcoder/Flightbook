e104239976353978cdcecdae35e7bb7be4c1db6e
pilotcoder <awings@o2.pl>
2023-09-29 07:48:05 +0200

    Documents repository update

--- src/main/java/pl/jgora/aeroklub/flightbook/repository/DocumentsRepository.java
+++ src/main/java/pl/jgora/aeroklub/flightbook/repository/DocumentsRepository.java
@@ -4,12 +4,17 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import pl.jgora.aeroklub.flightbook.entity.Documents;
 
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface DocumentsRepository extends JpaRepository<Documents, Long> {
 
     public Documents findByName(String name);
     public List<Documents> findDocumentsByGliderId (Long gliderId);
+
+    public List<Documents> findByDateOfExpiry (LocalDate dateOfExpiry);
+
+
 
 
 
